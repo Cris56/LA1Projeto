73d21ae4863a611003a5d3532546f26ef8952e34
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-12 21:34:52 +0000

    mostrar_tabuleiro
    Adicionada a função mostrar_tabuleiro completa

--- interface.c
+++ interface.c
@@ -5,9 +5,13 @@
 #include "logic.h"
 #include "interface.h"
 
-
 void mostrar_tabuleiro(ESTADO *e) { //Para imprimir o tabuleiro
-
+    for(int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%c", e->tab[j][i]);
+        }
+        printf("\n");
+    }
 }
 
 int interpretador(ESTADO *e) { //[A completar]
