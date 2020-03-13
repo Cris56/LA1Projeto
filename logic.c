44257a56b65ea4dfa792494cb76f214b69cb6640
jpvitor7 <62104562+jpvitor7@users.noreply.github.com>
2020-03-13 16:43:01 +0000

    Update logic.c

--- logic.c
+++ logic.c
@@ -6,6 +6,7 @@
 
 int jogar(ESTADO *e, COORDENADA c) { //[A completar]
     printf("jogar %d %d\n", c.coluna, c.linha);
+    e->tab[c.coluna][c.linha] = BRANCA;
     return 1;
 }
 //Esta função deverá receber o estado atual e uma coordenada e modificar o estado ao jogar na casa correta se a jogada for válida.
