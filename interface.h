c4e2b2d6b25fc48f721bf7561b0954ebb1fa550c
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-19 13:35:33 +0000

    Update interface.h
    Formatação de documentação

--- interface.h
+++ interface.h
@@ -1,4 +1,28 @@
+/**
+ * @file interface.h
+ * Definição das funções da parte da interface do programa
+ */
+
+#ifndef ___INTERFACE_H___
+#define  ___INTERFACE_H___
 #define BUF_SIZE 1024
 
+/**
+ * \brief Função que imprime o tabuleiro.
+ * @param e Apontador para o estado
+ */
 void mostrar_tabuleiro(ESTADO *e);
+
+/**
+ * \brief Interpretador de comandos.
+ *
+ * A função deve:
+ * 1. Ler uma linha (usando fgets)
+ * 2. Separar a linha por espaços (por exemplo utilizando sscanf ou strtok)
+ * 3. Conforme o comando a executar, chamar a função correspondente do interface ou da lógica do programa e depois do interface.
+ * @param e Apontador para o estado
+ * @return 1
+ */
 int interpretador(ESTADO *e);
+
+#endif
