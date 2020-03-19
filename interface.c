5b1c691dd8b8218320756c9834b525c6a5b8e0bc
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-19 12:04:46 +0000

    Update interface.c
    Alteração a mostrar_tabuleiro

--- interface.c
+++ interface.c
@@ -8,7 +8,7 @@
 void mostrar_tabuleiro(ESTADO *e) { //Para imprimir o tabuleiro
     for(int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            printf("%c", e->tab[j][i]);
+            printf("%c", obter_casa(e, coordenada(j, i)));
         }
         printf("\n");
     }
