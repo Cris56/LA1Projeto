b584abc7767fefb259062c4a1b6377ce9a5a31ea
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-19 20:58:59 +0000

    Update interface.c
    Alterações ao interpretador, gravar e ler

--- interface.c
+++ interface.c
@@ -7,6 +7,7 @@
 
 void mostrar_tabuleiro(ESTADO *e) { //Para imprimir o tabuleiro
     int n = 8;
+    printf("\n");
     for(int i = 0; i < 8; i++) {
         printf("%d ", n);
         n -= 1;
@@ -20,8 +21,7 @@ void mostrar_tabuleiro(ESTADO *e) { //Para imprimir o tabuleiro
 
 int interpretador(ESTADO *e) { //[A completar]
     char linha[BUF_SIZE];
-    char col[2], lin[2], cmd[10];
-    int n = 0;
+    char col[2], lin[2], cmd[50], arg[100], q[1];
     printf("# [%d] (%d) PL%d > ", obter_num_comandos(e), obter_numero_jogadas(e), obter_jogador_atual(e));
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
@@ -30,33 +30,29 @@ int interpretador(ESTADO *e) { //[A completar]
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
-    else {
-        sscanf(linha, "%s", cmd);
-        if (!strcmp(cmd, "Q")) exit(0);
-        if (!strcmp(cmd, "gr")) gravar(e);
-        if (!strcmp(cmd, "ler")) ler();
+    if (sscanf(linha, "%s", q) && (!strcmp(q, "Q"))) exit(0);
+    if (sscanf(linha, "%s %s", cmd, arg) == 2) {
+        if (!strcmp(cmd, "gr")) gravar(e, arg);
+        if (!strcmp(cmd, "ler")) ler(arg);
     }
     mais_comandos(e);
     return 1;
 }
 
-void gravar(ESTADO *e) {
+void gravar(ESTADO *e, char *nome) {
     FILE *fp;
-    fp = fopen("C:\\Users\\user\\CLionProjects\\ProjetoLA1\\FicheirosTeste\\teste.txt", "w");
-    int n = 8;
+    fp = fopen(nome, "w");
     for(int i = 0; i < 8; i++) {
-        fprintf(fp,"%d ", n);
-        n -= 1;
         for (int j = 0; j < 8; j++) {
             fprintf(fp,"%c", obter_casa(e, coordenada(j, i)));
         }
         fprintf(fp,"\n");
     }
-    fprintf(fp,"  abcdefgh\n");
+    fprintf(fp,"\n");
     fclose(fp);
 }
 
-void ler() {
+void ler(char *nome) {
     FILE *fp;
-    fp = fopen("C:\\Users\\user\\CLionProjects\\ProjetoLA1\\FicheirosTeste\\teste.txt", "r");
+    fp = fopen(nome, "r");
 }
