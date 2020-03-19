7844b1480ee1856eaba317e6121b49bedeb6de30
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-19 11:59:04 +0000

    Update data.c
    obter & mudar casa; coordenada; inicializar_estado alterado

--- data.c
+++ data.c
@@ -8,10 +8,12 @@ ESTADO *inicializar_estado() { //Esta função deverá criar um estado vazio (co
     e->jogador_atual = 1;
     e->num_jogadas = 0;
     for(int i = 0; i < 8; i++)
-        for(int j = 0; j < 8; j++) e->tab[j][i] = VAZIO;
-    e->tab[0][7] = 49;
-    e->tab[7][0] = 50;
-    e->tab[4][3] = BRANCA;
+        for(int j = 0; j < 8; j++) {
+            mudar_casa(e, coordenada(j, i), VAZIO);
+        }
+    mudar_casa(e, coordenada(0, 7), UM);
+    mudar_casa(e, coordenada(7, 0), DOIS);
+    mudar_casa(e, coordenada(4, 3), BRANCA);
     return e;
 }
 
@@ -24,5 +26,18 @@ int obter_numero_de_jogadas(ESTADO *estado) { //Esta função permite obter quan
 
 }
 
-CASA obter_estado_casa(ESTADO *e, COORDENADA c) { //Esta função permite obter o estado atual da casa
+CASA obter_casa(ESTADO *e, COORDENADA c) { //Esta função permite obter o estado atual da casa
+    CASA casa = e->tab[c.coluna][c.linha];
+    return casa;
+}
+
+void mudar_casa(ESTADO *e, COORDENADA c, CASA A) { //Muda o estado da casa
+    e->tab[c.coluna][c.linha] = A;
+}
+
+COORDENADA coordenada(int j, int i) { //Transforma dois inteiros i e j numa coordenada
+    COORDENADA c;
+    c.coluna = j;
+    c.linha = i;
+    return c;
 }
