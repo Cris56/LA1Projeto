16ab64ab30fc21f53d58c69ab75b97cb3e12672c
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-22 19:00:12 +0000

    Update logic.h
    Adicionar protótipos e respetiva documentação

--- logic.h
+++ logic.h
@@ -25,4 +25,28 @@ int jogar(ESTADO *e, COORDENADA c);
  */
 int verifica_jogada(ESTADO *e, COORDENADA c);
 
+/**
+ * \brief Verifica se o jogo deve acabar.
+ * @param e Apontador para o estado
+ * @param c Coordenada da última jogada
+ * @return 1 se o jogo deverá acabar, 0 se deverá continuar
+ */
+int fimdojogo(ESTADO *e, COORDENADA c);
+
+/**
+ * \brief Verifica se as casas vizinhas estão cheias.
+ * @param e Apontador para o estado
+ * @param c Coordenada da última jogada
+ * @return 0 se alguma está vazia, 1 se estão todas cheias
+ */
+int vizinhas_cheias(ESTADO *e, COORDENADA c);
+
+/**
+ * \brief Verifica se a casa é uma casa final.
+ * @param e Apontador para o estado
+ * @param c A coordenada da casa
+ * @return 1 ou 2, consoante a casa final, ou 0 se não o é
+ */
+int casa_final(COORDENADA c);
+
 #endif
