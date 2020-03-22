3c6d7c79464f72ea57c20e1fb986307a09e91e5d
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-22 19:02:19 +0000

    Update interface.c
    Alteração a funções; Adicionar vencedor

--- interface.c
+++ interface.c
@@ -5,12 +5,12 @@
 #include "logic.h"
 #include "interface.h"
 
-void mostrar_tabuleiro(ESTADO *e) { //Para imprimir o tabuleiro
-    int n = 8;
+void mostrar_tabuleiro(ESTADO *e) {
+    int n = 1;
     printf("\n");
-    for(int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
         printf("%d ", n);
-        n -= 1;
+        n += 1;
         for (int j = 0; j < 8; j++) {
             printf("%c", obter_casa(e, coordenada(j, i)));
         }
@@ -23,17 +23,23 @@ int interpretador(ESTADO *e) { //[A completar]
     char linha[BUF_SIZE];
     char col[2], lin[2], cmd[50], arg[100], q[1];
     printf("# [%d] (%d) PL%d > ", obter_num_comandos(e), obter_numero_jogadas(e), obter_jogador_atual(e));
-    if(fgets(linha, BUF_SIZE, stdin) == NULL)
+    if (fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
-    if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
+    if (strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
         COORDENADA coord = {*col - 'a', *lin - '1'};
         jogar(e, coord);
         mostrar_tabuleiro(e);
+        if (fimdojogo(e, coord)) {
+            vencedor(e, coord);
+            exit(0);
+        }
     }
-    if (sscanf(linha, "%s", q) && (!strcmp(q, "Q"))) exit(0);
-    if (sscanf(linha, "%s %s", cmd, arg) == 2) {
-        if (!strcmp(cmd, "gr")) gravar(e, arg);
-        if (!strcmp(cmd, "ler")) ler(arg);
+    else {
+        if (sscanf(linha, "%s", q) && (!strcmp(q, "Q"))) exit(0);
+        if (sscanf(linha, "%s %s", cmd, arg) == 2) {
+            if (!strcmp(cmd, "gr")) gravar(e, arg);
+            if (!strcmp(cmd, "ler")) ler(arg);
+        }
     }
     mais_comandos(e);
     return 1;
@@ -42,7 +48,7 @@ int interpretador(ESTADO *e) { //[A completar]
 void gravar(ESTADO *e, char *nome) {
     FILE *fp;
     fp = fopen(nome, "w");
-    for(int i = 0; i < 8; i++) {
+    for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
             fprintf(fp,"%c", obter_casa(e, coordenada(j, i)));
         }
@@ -55,4 +61,14 @@ void gravar(ESTADO *e, char *nome) {
 void ler(char *nome) {
     FILE *fp;
     fp = fopen(nome, "r");
+}
+
+void vencedor(ESTADO *e, COORDENADA c) {
+    int v = 0;
+    int j = obter_jogador_atual(e);
+    int f = casa_final(c);
+    if (f == 1) v = 1;
+    else if (f == 2) v = 2;
+    else v = j;
+    printf("O vencedor e' o jogador %d! Parabens.\n", v);
 }
