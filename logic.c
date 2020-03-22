7e1dd48375eed291dba96f7ec80725e294fa601f
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-03-22 18:58:41 +0000

    Update logic.c
    Alterações a funções; Adicionar fimdojogo, vizinhas_cheias, casa_final

--- logic.c
+++ logic.c
@@ -4,13 +4,12 @@
 #include "data.h"
 #include "logic.h"
 
-int jogar(ESTADO *e, COORDENADA c) { //[A completar]
+int jogar(ESTADO *e, COORDENADA c) {
     int p = verifica_jogada(e, c);
-    if(p == 1) {
-        COORDENADA ult_jog = obter_ultima_jogada(e);
+    if (p == 1) {
         mudar_casa(e, c,BRANCA);
-        mudar_casa(e, ult_jog,PRETA);
-        e->ultima_jogada = c;
+        mudar_casa(e, obter_ultima_jogada(e),PRETA);
+        mudar_ultima_jogada(e, c);
         return 1;
     }
     return 0;
@@ -26,7 +25,32 @@ int verifica_jogada(ESTADO *e, COORDENADA c) {
     int difcol = (col1 - col2);
     int diflin = (lin1 - lin2);
     if ((difcol * difcol) == 0 && (diflin * diflin) == 0) return 0;
-    if (((difcol) * (difcol) <= 1) && ((diflin) * (diflin) <= 1) && (obter_casa(e, c) == VAZIO))
+    if (((difcol) * (difcol) <= 1) && ((diflin) * (diflin) <= 1) && (obter_casa(e, c) != PRETA) && obter_casa(e, c) != BRANCA)
         return 1;
+    else return 0;
+}
+
+int fimdojogo(ESTADO *e, COORDENADA c) {
+    if (casa_final(c) == 1 || casa_final(c) == 2 || vizinhas_cheias(e, c)) {
+        return 1;
+    }
+    else return 0;
+}
+
+int vizinhas_cheias(ESTADO *e, COORDENADA c) {
+    int col = c.coluna, lin = c.linha;
+    for (int i = -1; i < 2; i++){
+        for (int j = -1; j < 2; j++) {
+            if (i != 0 && j != 0) {
+                if (obter_casa(e, coordenada(col + j, lin + i)) == VAZIO) return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int casa_final(COORDENADA c) {
+    if (c.coluna == 0 && c.linha == 7) return 1;
+    else if (c.coluna == 7 && c.linha == 0) return 2;
     else return 0;
 }
