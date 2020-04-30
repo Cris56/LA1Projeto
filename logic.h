dea0b7efd3477edc0a5d6b51ef46740011e8a1c4
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-04-30 17:33:52 +0100

    Adicionar protótipos e documentação/Cleanup

--- logic.h
+++ logic.h
@@ -17,6 +17,33 @@
  */
 int jogar(ESTADO *e, COORDENADA c);
 
+
+
+/**
+ * \brief Retorna o jogo à posição a seguir ao final da jogada indicada.
+ * @param e Apontador para o estado
+ * @param a O número da jogada
+ */
+void pos(ESTADO *e, int a);
+
+
+
+/**
+ * \brief Faz uma jogada pelo jogador (estratégia #1).
+ * @param e Apontador para o estado
+ * @return 1
+ */
+int jog(ESTADO *e);
+
+/**
+ * \brief Faz uma jogada pelo jogador (estratégia #2).
+ * @param e Apontador para o estado
+ * @return 1
+ */
+int jog2(ESTADO *e);
+
+
+
 /**
  * \brief Verifica se a jogada indicada é válida.
  * @param e Apontador para o estado
@@ -24,6 +51,15 @@ int jogar(ESTADO *e, COORDENADA c);
  * @return 1 se a jogada for possível, 0 se não for
  */
 int verifica_jogada(ESTADO *e, COORDENADA c);
+
+/**
+ * \brief Altera o jogador atual e jogadas.
+ * @param e Apontador para o estado
+ * @param c A coordenada da jogada feita
+ */
+void jog_jogadas(ESTADO *e, COORDENADA c);
+
+
 
 /**
  * \brief Verifica se o jogo deve acabar.
@@ -34,14 +70,6 @@ int verifica_jogada(ESTADO *e, COORDENADA c);
 int fimdojogo(ESTADO *e, COORDENADA c);
 
 /**
- * \brief Verifica se as casas vizinhas estão cheias.
- * @param e Apontador para o estado
- * @param c Coordenada da última jogada
- * @return 0 se alguma está vazia, 1 se estão todas cheias
- */
-int vizinhas_cheias(ESTADO *e, COORDENADA c);
-
-/**
  * \brief Verifica se a casa é uma casa final.
  * @param e Apontador para o estado
  * @param c A coordenada da casa
@@ -50,18 +78,40 @@ int vizinhas_cheias(ESTADO *e, COORDENADA c);
 int casa_final(COORDENADA c);
 
 /**
- * \brief Altera o jogador atual e jogadas.
+ * \brief Verifica se as casas vizinhas estão cheias.
  * @param e Apontador para o estado
- * @param c A coordenada da jogada feita
+ * @param c Coordenada da última jogada
+ * @return 0 se alguma está vazia, 1 se estão todas cheias
  */
-void jog_jogadas(ESTADO *e, COORDENADA c);
+int vizinhas_cheias(ESTADO *e, COORDENADA c);
 
 /**
- * \brief Retorna o jogo à posição a seguir ao final da jogada indicada.
+ * \brief Determina e congratula o vencedor.
  * @param e Apontador para o estado
- * @param a O número da jogada
+ * @param c A coordenada da jogada
  */
-void pos(ESTADO *e, int a);
+int vencedor(ESTADO *e, COORDENADA c);
+
+
+
+/**
+ * \brief Calcula se uma coordenada está mais próxima da casa final do que outra.
+ * @param j Jogador atual
+ * @param jogada Coordenada a ser verificada
+ * @param atual Coordenada que será a jogada final
+ * @return Verdadeiro se a que foi verificada está mais próxima
+ */
+int maisproximo(int j, COORDENADA jogada, COORDENADA atual);
+
+/**
+ * \brief Calcula a distância entre duas coordenadas (número de posições a percorrer).
+ * @param a Coordenada a
+ * @param b Coordenada b
+ * @return A distância entre as duas coordenadas
+ */
+int distancia(COORDENADA a, COORDENADA b);
+
+
 
 /**
  * \brief Transforma dois caracteres em coordenada válida.
@@ -69,5 +119,12 @@ void pos(ESTADO *e, int a);
  * @param l Caracter linha
  */
 COORDENADA chars_para_coord(char c, char l);
+
+/**
+ * \brief Duplica uma coordenada (devolve o apontador para a mesma e aloca memória).
+ * @param c Coordenada a duplicar
+ * @return Apontador para a coordenada
+ */
+COORDENADA *duplicar_coordenada(COORDENADA c);
 
 #endif
