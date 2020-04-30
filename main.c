d19712bb312118ba7382d7c045fbf6f4ff3df995
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-04-30 17:35:16 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 #include "data.h"
-#include "logic.h"
 #include "interface.h"
 
 int main() {
+    srand(time(NULL));
     ESTADO *e = inicializar_estado();
+    mostrar_tabuleiro(e);
     while(interpretador(e));
     return 0;
 }
