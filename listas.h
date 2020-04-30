57467a592a320aa89ace6d9deb4259c6a97ad387
Cristiana Tamborino Ribeiro <ctr56@live.com.pt>
2020-04-30 13:34:53 +0100

    Adicionada documentação

--- listas.h
+++ listas.h
@@ -3,22 +3,57 @@
  * Módulo das listas do programa
  */
 
+#ifndef __LISTAS_H___
 #define BUF_SIZE 1024
 
+/**
+ * \brief Tipo de dados para as listas.
+ */
 typedef struct nodo {
     void *valor;
     struct nodo *proximo;
 } NODO, *LISTA;
 
-// Cria uma lista vazia
+/**
+ * \brief Cria uma lista vazia.
+ * @return Lista criada
+ */
 LISTA criar_lista();
-// Insere um valor na cabeça da lista
+
+/**
+ * \brief Insere um valor na cabeça da lista.
+ * @param L Lista onde será inserida a cabeça
+ * @param valor Valor a inserir na cabeça
+ * @return O nodo head
+ */
 LISTA insere_cabeca(LISTA L, void *valor);
-// Devolve a cabeça da lista
+
+/**
+ * \brief Devolve a cabeça da lista.
+ * @param L Lista em questão
+ * @return Apontador para a cabeça
+ */
 void *devolve_cabeca(LISTA L);
-// Devolve a cauda da lista
-LISTA proximo(LISTA L);
-// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+
+/**
+ * \brief Devolve a cauda da lista.
+ * @param L Lista em questão
+ * @return A cauda da lista
+ */
+LISTA devolve_proximo(LISTA L);
+
+/**
+ * \brief Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda.
+ * @param L Lista em questão
+ * @return A cauda da lista
+ */
 LISTA remove_cabeca(LISTA L);
-// Devolve verdareiro se a lista é vazia
+
+/**
+ * \brief Devolve verdadeiro se a lista é vazia.
+ * @param L Lista em questão
+ * @return Verdadeiro (1) se a lista está vazia
+ */
 int lista_esta_vazia(LISTA L);
+
+#endif
